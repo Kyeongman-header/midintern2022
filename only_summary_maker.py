899a9009cc218f6863aea29db57a97da47b0b40a
Kyeongman-header <zzangman2@gmail.com>
2022-11-09 22:44:30 +0900

    bert summary maker

--- only_summary_maker.py
+++ only_summary_maker.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 #tf.config.set_visible_devices([], 'GPU')
 from summary_maker import *
+from bert_summary_maker import *
 import consts
 import torch
  
@@ -14,10 +15,12 @@ torch.cuda.device(0)
 #import os
 #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-RANGE=consts.BATCH_SIZE * 5000 # if '0', then whole range is target.
+RANGE=consts.BATCH_SIZE * 10000 # if '0', then whole range is target.
 
-TRAIN_FILE="_sm_train_whole"
-VALID_FILE="_sm_valid_whole"
+TRAIN_FILE="ext_18_train"
+VALID_FILE="ext_18_valid"
 FURTHER_TRAIN=True
-summary_maker(START=0,RANGE=RANGE,seq_length=0,file=TRAIN_FILE,is_model_or_given_dataset=True,device=0)
-summary_maker(START=0,RANGE=RANGE,seq_length=0,file=VALID_FILE,is_model_or_given_dataset=True,device=0)
+bert_summary_maker(START=0,RANGE=RANGE,seq_length=18,file=TRAIN_FILE,is_model_or_given_dataset=True,device=0)
+bert_summary_maker(START=0,RANGE=RANGE,seq_length=18,file=VALID_FILE,is_model_or_given_dataset=True,device=0)
+#summary_maker(START=0,RANGE=RANGE,seq_length=18,file=TRAIN_FILE,is_model_or_given_dataset=True,device=0)
+#summary_maker(START=0,RANGE=RANGE,seq_length=18,file=VALID_FILE,is_model_or_given_dataset=True,device=0)
